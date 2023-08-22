3a2f49e6e109bfe3cb69bf3935ed1980ca3f8a07
Ryle Edwards <ryle.edwards@gmail.com>
2023-08-21 21:19:57 -0500

    Add move() function and evaluate validity of requested move

--- knight.js
+++ knight.js
@@ -1,19 +1,27 @@
 import { Gameboard } from './gameboard.js';
 export class Knight {
-  constructor(x = 1, y = 0, gameboard) {
+  constructor(gameboard, x = 1, y = 0) {
+    this.gameboard = gameboard;
     this.x = x;
     this.y = y;
   }
-  move(newX, newY) {
+  move(newX, newY, oldX = this.x, oldY = this.y, gameboard = this.gameboard) {
     const evalMove = function (newX, newY) {
       if (newX < 0 || newY < 0) return false;
-      if (Math.abs(this.x - newX) !== 1) return false;
-      if (Math.abs(this.y - newY) !== 2) return false;
+      if (Math.abs(oldX - newX) !== 1) return false;
+      if (Math.abs(oldY - newY) !== 2) return false;
       if (newX > gameboard.width - 1) return false;
       if (newY > gameboard.height - 1) return false;
       return true;
     };
-    if (evalMove) {
-    }
+    if (evalMove(newX, newY)) {
+      let oldX = this.x;
+      let oldY = this.y;
+      this.x = newX;
+      this.y = newY;
+      console.log(`[${oldX},${oldY}] -> [${this.x},${this.y}]`);
+    } else console.log('Illegal move requested');
   }
+
+  knightMoves(start, end) {}
 }
