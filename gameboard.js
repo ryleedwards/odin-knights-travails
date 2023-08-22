6b1a53fabeb0038db9b43283d1bbb56d34bfbc15
Ryle Edwards <ryle.edwards@gmail.com>
2023-08-21 21:20:22 -0500

    Remove move() function

--- gameboard.js
+++ gameboard.js
@@ -1,12 +1,6 @@
 export class Gameboard {
-  constructor(height = 8, width = 8) {
+  constructor(height = 7, width = 7) {
     this.height = height;
     this.width = width;
-  }
-  move(newX, newY) {
-    const evalMove = function (newX, newY) {
-      if (Math.abs(this.x - newX) !== 1) return false;
-      if (Math.abs(this.y - newY) !== 3) return false;
-    };
   }
 }
