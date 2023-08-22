ae001ef54c8a0d2c67c90e695830d56a4f14345f
Ryle Edwards <ryle.edwards@gmail.com>
2023-08-21 19:22:55 -0500

    Add constructor and base logic for move() and evalMove() funcs

--- knight.js
+++ knight.js
@@ -1,3 +1,19 @@
+import { Gameboard } from './gameboard.js';
 export class Knight {
-  constructor() {}
+  constructor(x = 1, y = 0, gameboard) {
+    this.x = x;
+    this.y = y;
+  }
+  move(newX, newY) {
+    const evalMove = function (newX, newY) {
+      if (newX < 0 || newY < 0) return false;
+      if (Math.abs(this.x - newX) !== 1) return false;
+      if (Math.abs(this.y - newY) !== 2) return false;
+      if (newX > gameboard.width - 1) return false;
+      if (newY > gameboard.height - 1) return false;
+      return true;
+    };
+    if (evalMove) {
+    }
+  }
 }
