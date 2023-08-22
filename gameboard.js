c352ef62776925d8d639e1d26c69966f8c14d8c5
Ryle Edwards <ryle.edwards@gmail.com>
2023-08-21 19:23:05 -0500

    Add constructor

--- gameboard.js
+++ gameboard.js
@@ -1,3 +1,12 @@
 export class Gameboard {
-  constructor() {}
+  constructor(height = 8, width = 8) {
+    this.height = height;
+    this.width = width;
+  }
+  move(newX, newY) {
+    const evalMove = function (newX, newY) {
+      if (Math.abs(this.x - newX) !== 1) return false;
+      if (Math.abs(this.y - newY) !== 3) return false;
+    };
+  }
 }
