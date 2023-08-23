baf799386101b1d77848613393ef5f309ba35da6
Ryle Edwards <ryle.edwards@gmail.com>
2023-08-23 16:21:21 -0500

    Restart to work off app.js alone
    Restarting in order to simplify app logic.
    No longer using modules
    Board defined in app.js
    Knight not an object, rather just tracked position on board
    generateMoves() complete
    isPositionOnBoard() complete

--- app.js
+++ app.js
@@ -1,2 +1,37 @@
-import { Knight } from './knight.js';
-import { Gameboard } from './gameboard.js';
+const board = [];
+const rows = 8;
+const cols = 8;
+
+for (let i = 0; i < rows; i++) {
+  board[i] = [];
+  for (let j = 0; j < cols; j++) {
+    board[i][j] = 'o';
+  }
+}
+
+// Given a knight placement on board, what are its available moves?
+// Return as an array of edges
+const generateMoves = (board, r, c) => {
+  const moves = [];
+  if (isPositionOnBoard(r + 1, c + 2)) moves.push([r + 1, c + 2]);
+  if (isPositionOnBoard(r - 1, c + 2)) moves.push([r - 1, c + 2]);
+  if (isPositionOnBoard(r + 1, c - 2)) moves.push([r + 1, c - 2]);
+  if (isPositionOnBoard(r - 1, c - 2)) moves.push([r - 1, c - 2]);
+  if (isPositionOnBoard(r + 2, c + 1)) moves.push([r + 2, c + 1]);
+  if (isPositionOnBoard(r - 2, c + 1)) moves.push([r - 2, c + 1]);
+  if (isPositionOnBoard(r + 2, c - 1)) moves.push([r + 2, c - 1]);
+  if (isPositionOnBoard(r - 2, c - 1)) moves.push([r - 2, c - 1]);
+  return moves;
+};
+
+const isPositionOnBoard = (r, c) => {
+  if (r < 0 || r > rows - 1 || c < 0 || c > cols - 1) return false;
+  else return true;
+};
+
+let r = 1;
+let c = 6;
+console.log(`Moves for [${r},${c}] :`);
+generateMoves(board, r, c).forEach((result) => {
+  console.log(result);
+});
